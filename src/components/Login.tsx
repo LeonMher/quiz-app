4238d3946cf3e36b9dd219fc5403239115ab2d9f
LeonMher <99.p3rc3n7@gmail.com>
2024-07-27 20:21:53 +0400

    implement basic react form hook for the login component

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,76 +1,49 @@
 import { useState } from "react";
 import { auth } from "../api/firebase";
 import { signInWithEmailAndPassword } from "firebase/auth";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type TInputForm = {
+  email: string;
+  password: string;
+};
 
 const Login = () => {
+  const [notice, setNotice] = useState<string>("");
   const navigate = useNavigate();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [notice, setNotice] = useState("");
 
-  const loginWithUsernameAndPassword = async (e) => {
-    e.preventDefault();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TInputForm>();
 
+  const onSubmit: SubmitHandler<TInputForm> = async (data) => {
     try {
-      await signInWithEmailAndPassword(auth, email, password);
+      await signInWithEmailAndPassword(auth, data.email, data.password);
       navigate("./profile");
     } catch {
-      setNotice("You entered a wrong username or password.");
+      setNotice("wrong credentials");
     }
   };
 
   return (
-    <div className="container">
-      <div className="row justify-content-center">
-        <form className="col-md-4 mt-3 pt-3 pb-3">
-          {"" !== notice && (
-            <div className="alert alert-warning" role="alert">
-              {notice}
-            </div>
-          )}
-          <div className="form-floating mb-3">
-            <input
-              type="email"
-              className="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-              placeholder="name@example.com"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            ></input>
-            <label htmlFor="exampleInputEmail1" className="form-label">
-              Email address
-            </label>
-          </div>
-          <div className="form-floating mb-3">
-            <input
-              type="password"
-              className="form-control"
-              id="exampleInputPassword1"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            ></input>
-            <label htmlFor="exampleInputPassword1" className="form-label">
-              Password
-            </label>
-          </div>
-          <div className="d-grid">
-            <button
-              type="submit"
-              className="btn btn-primary pt-3 pb-3"
-              onClick={(e) => loginWithUsernameAndPassword(e)}
-            >
-              Submit
-            </button>
-          </div>
-          <div className="mt-3 text-center">
-            <span>
-              Need to sign up for an account?{" "}
-              <Link to="./signup">Click here.</Link>
-            </span>
-          </div>
+    <div>
+      <div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            type="text"
+            {...register("email", { required: "Email is required" })}
+          />
+          {errors.email && <span>{errors.email.message}</span>}
+          <input
+            type="text"
+            {...register("password", { required: "Password is required" })}
+          />
+          {errors.password && <span>{errors.password.message}</span>}
+          <p>{notice}</p>
+          <input type="submit" />
         </form>
       </div>
     </div>
